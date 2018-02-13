8373b79482f6d961186c0c06e46b052ce0af0ac4
Vipul Sharma <vipul.sharma@trantorinc.com>
2018-02-13 13:42:22 +0530

    Modified code to accept cards as input and added try catch block to handle errors

--- server/server.js
+++ server/server.js
@@ -1,34 +1,57 @@
 const readlineSync = require("readline-sync");
 const { Common } = require("./utils/common");
 const common = new Common();
-const chalk = require('chalk');
+const chalk = require("chalk");
 
 if (readlineSync.keyInYN("Do you want to play Texas Holdem?")) {
-  console.log(chalk.green("Staring now..."));
-  readlineSync.question("Get 5 community cards?");
-  const communityCards = common.fillDeck(5);
-  console.log(common.displayCards(communityCards) + "\n");
-  const playersCount = readlineSync.question("Enter number of players? ");
-  console.log("\n");
-  if (playersCount) {
-    let players = [];
-    for (let i = 0; i < playersCount; i++) {
-      const playerName = readlineSync.question(`Enter Player ${i + 1}: `);
-      const playerHand = common.fillDeck(2);
-      let playerData = {
-        name: playerName,
-        cards: playerHand
+  try {
+    console.log(chalk.green("Starting now..."));
+    const communityCards = readlineSync
+      .question("Get 5 community cards? ")
+      .replace(/(\S)(\S)/g, function($$, $1, $2) {
+        return $1 + $2.toLowerCase();
+      })
+      .split(" ");
+    console.log(common.displayCards(communityCards) + "\n");
+    const playersCount = readlineSync.question("Enter number of players? ");
+    console.log("\n");
+    if (playersCount) {
+      let players = [];
+      for (let i = 0; i < playersCount; i++) {
+        let playerName = [],
+          playerHand = [];
+        const playerInput = readlineSync
+          .question(`Enter Player ${i + 1}: `)
+          .split(" ")
+          .map((input, i) => {
+            if (i === 0) {
+              return playerName.push(input);
+            } else {
+              return playerHand.push(
+                input.replace(/(\S)(\S)/g, function($$, $1, $2) {
+                  return $1 + $2.toLowerCase();
+                })
+              );
+            }
+          });
+
+        players.push({
+          name: playerName,
+          cards: playerHand
+        });
+        console.log(`${playerName} ${common.displayCards(playerHand)}\n`);
+      }
+      const input = {
+        communityCards,
+        players
       };
-      players.push(playerData);
-      console.log(`${playerName} ${common.displayCards(playerHand)}\n`);
+
+      const result = common.evaluateHands(input);
+      common.output(result);
     }
-    const input = {
-      communityCards,
-      players
-    };
-    const result = common.evaluateHands(input);
-    common.output(result);
+  } catch (e) {
+    console.log(chalk.red(`Error: ${e.message}`));
   }
 } else {
-  console.log("Good bye...");
+  console.log(chalk.green("Good bye..."));
 }
