ac65b86482fa849e9984a2d6efae60cc1682ee05
vipultrantor <vipul.sharma@trantorinc.com>
2018-02-14 16:56:16 +0530

    Fixed errors using Eslint & Airbnb JavaScript guidelines

--- server/server.js
+++ server/server.js
@@ -1,48 +1,41 @@
-const readlineSync = require("readline-sync");
-const { displayCards, evaluateHands, output } = require("./utils/common");
-const chalk = require("chalk");
+const readlineSync = require('readline-sync');
+const { displayCards, evaluateHands, output } = require('./utils/common');
+const chalk = require('chalk');
 
-if (readlineSync.keyInYN("Do you want to play Texas Holdem?")) {
+if (readlineSync.keyInYN('Do you want to play Texas Holdem?')) {
   try {
-    console.log(chalk.green("Starting now..."));
+    console.log(chalk.green('Starting now...'));
     const communityCards = readlineSync
-      .question("Get 5 community cards? ")
-      .replace(/(\S)(\S)/g, function($$, $1, $2) {
-        return $1 + $2.toLowerCase();
-      })
-      .split(" ");
-    console.log(displayCards(communityCards) + "\n");
-    const playersCount = readlineSync.question("Enter number of players? ");
-    console.log("\n");
+      .question('Get 5 community cards? ')
+      .replace(/(\S)(\S)/g, ($$, $1, $2) => $1 + $2.toLowerCase())
+      .split(' ');
+    console.log(`${displayCards(communityCards)}\n`);
+    const playersCount = readlineSync.question('Enter number of players? ');
+    console.log('\n');
     if (playersCount) {
-      let players = [];
-      for (let i = 0; i < playersCount; i++) {
-        let playerName = [],
-          playerHand = [];
-        const playerInput = readlineSync
+      const players = [];
+      for (let i = 0; i < playersCount; i += 1) {
+        const playerName = [];
+        const playerHand = [];
+        readlineSync
           .question(`Enter Player ${i + 1}: `)
-          .split(" ")
-          .map((input, i) => {
-            if (i === 0) {
+          .split(' ')
+          .map((input, k) => {
+            if (k === 0) {
               return playerName.push(input);
-            } else {
-              return playerHand.push(
-                input.replace(/(\S)(\S)/g, function($$, $1, $2) {
-                  return $1 + $2.toLowerCase();
-                })
-              );
             }
+            return playerHand.push(input.replace(/(\S)(\S)/g, ($$, $1, $2) => $1 + $2.toLowerCase()));
           });
 
         players.push({
           name: playerName,
-          cards: playerHand
+          cards: playerHand,
         });
         console.log(`${playerName} ${displayCards(playerHand)}\n`);
       }
       const input = {
         communityCards,
-        players
+        players,
       };
 
       const result = evaluateHands(input);
@@ -52,5 +45,5 @@ if (readlineSync.keyInYN("Do you want to play Texas Holdem?")) {
     console.log(chalk.red(`Error: ${e.message}`));
   }
 } else {
-  console.log(chalk.green("Good bye..."));
+  console.log(chalk.green('Good bye...'));
 }
